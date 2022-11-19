a8b0a3b2a2c7ff6c951a05193df8bb03c2b153d3
amitbaliyan0007 <amitkumar2511nov@gmail.com>
2022-11-19 14:50:12 +0530

    my commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:117068767851:web:8001f4529c789aea49472e";
+  static String apiKey = "AIzaSyADPDxdhvLxD5c1WlApjf0kZo-Nw0IZCWY";
+  static String messagingSenderId = "117068767851";
+  static String projectId = "groupchatapp-10907";
+  final primaryColor =  Colors.teal.shade300;
+}
